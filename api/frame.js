6bbc57443c32d3281f5b1f2e91ee7d4ce015f822
Mukedlii <107921705+Mukedlii@users.noreply.github.com>
2025-11-15 07:21:08 +0100

    Refactor GET function to use new Response format

--- api/frame.js
+++ api/frame.js
@@ -1,19 +1,30 @@
-export async function GET() {
-  return Response.json({
-    accepts: ["https://amoba-frame.vercel.app"],
+export const config = {
+  runtime: "edge"
+};
 
-    frames: [
-      {
-        version: "vNext",
-        image: "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
-        buttons: [
-          {
-            action: "post",
-            label: "Start Game",
-            target: "https://amoba-frame.vercel.app/api/start"
-          }
-        ]
+export async function GET() {
+  return new Response(
+    JSON.stringify({
+      accepts: ["https://amoba-frame.vercel.app"],
+      frames: [
+        {
+          version: "vNext",
+          image: "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
+          buttons: [
+            {
+              action: "post",
+              label: "Start Game",
+              target: "https://amoba-frame.vercel.app/api/start"
+            }
+          ]
+        }
+      ]
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store"
       }
-    ]
-  });
+    }
+  );
 }
