49defe4577313c3a2fd6172b8e55254782593600
Mukedlii <107921705+Mukedlii@users.noreply.github.com>
2025-11-15 06:49:17 +0100

    Refactor response structure in handler function

--- api/start.js
+++ api/start.js
@@ -1,15 +1,17 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    "frames": {
-      "version": "vNext",
-      "image": "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
-      "buttons": [
-        {
-          "action": "post",
-          "label": "Next move",
-          "target": "https://amoba-frame.vercel.app/api/play"
-        }
-      ]
-    }
+  return res.status(200).json({
+    frames: [
+      {
+        version: "vNext",
+        image: "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
+        buttons: [
+          {
+            action: "post",
+            label: "Your move",
+            target: "https://amoba-frame.vercel.app/api/play"
+          }
+        ]
+      }
+    ]
   });
 }
