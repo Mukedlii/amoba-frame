0fb341a9032b6517c7301d7c164d93fbedad991d
Mukedlii <107921705+Mukedlii@users.noreply.github.com>
2025-11-15 07:40:59 +0100

    Update frame.js

--- api/frame.js
+++ api/frame.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Cache-Control", "no-store");
+  res.setHeader("X-Frame-Options", "allow");
 
   res.status(200).json({
     accepts: ["https://amoba-frame.vercel.app"],
