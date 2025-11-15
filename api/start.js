31015484a5b20f4139ee6aec258e1be050d6f072
Mukedlii <107921705+Mukedlii@users.noreply.github.com>
2025-11-15 08:03:06 +0100

    Update start.js

--- api/start.js
+++ api/start.js
@@ -1,17 +1,8 @@
 export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/json");
+
   res.status(200).json({
-    frames: [
-      {
-        version: "vNext",
-        image: "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
-        buttons: [
-          {
-            action: "post",
-            label: "Next move",
-            target: "https://amoba-frame.vercel.app/api/frame"
-          }
-        ]
-      }
-    ]
+    message: "Game started",
+    nextFrame: "https://amoba-frame.vercel.app/api/play"
   });
 }
