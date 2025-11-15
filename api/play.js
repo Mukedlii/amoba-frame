7a6dd3425a28975a0e8560727db79a37feb97c5c
Mukedlii <107921705+Mukedlii@users.noreply.github.com>
2025-11-15 06:43:55 +0100

    Add API handler for play functionality

---
+++ api/play.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    "frames": {
+      "version": "vNext",
+      "image": "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
+      "buttons": [
+        {
+          "action": "post",
+          "label": "Again",
+          "target": "https://amoba-frame.vercel.app/api/frame"
+        }
+      ]
+    }
+  });
+}
