643755d73d8a7b3e6cc2a867f70e5120e3ba36dc
Mukedlii <107921705+Mukedlii@users.noreply.github.com>
2025-11-15 06:59:14 +0100

    Refactor frame response structure to use array

--- api/frame.js
+++ api/frame.js
@@ -1,15 +1,17 @@
 export default function handler(req, res) {
   res.status(200).json({
-    "frames": {
-      "version": "vNext",
-      "image": "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
-      "buttons": [
-        {
-          "action": "post",
-          "label": "Start Game",
-          "target": "https://amoba-frame.vercel.app/api/start"
-        }
-      ]
-    }
+    frames: [
+      {
+        version: "vNext",
+        image: "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
+        buttons: [
+          {
+            action: "post",
+            label: "Start Game",
+            target: "https://amoba-frame.vercel.app/api/start"
+          }
+        ]
+      }
+    ]
   });
 }
