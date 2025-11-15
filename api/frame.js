6edf42076716a085c1a1229dd3d7185487828d27
Mukedlii <107921705+Mukedlii@users.noreply.github.com>
2025-11-15 07:31:45 +0100

    Refactor GET handler to use res.json method

--- api/frame.js
+++ api/frame.js
@@ -1,30 +1,21 @@
-export const config = {
-  runtime: "edge"
-};
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Cache-Control", "no-store");
 
-export async function GET() {
-  return new Response(
-    JSON.stringify({
-      accepts: ["https://amoba-frame.vercel.app"],
-      frames: [
-        {
-          version: "vNext",
-          image: "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
-          buttons: [
-            {
-              action: "post",
-              label: "Start Game",
-              target: "https://amoba-frame.vercel.app/api/start"
-            }
-          ]
-        }
-      ]
-    }),
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "no-store"
+  res.status(200).json({
+    accepts: ["https://amoba-frame.vercel.app"],
+    frames: [
+      {
+        version: "vNext",
+        image: "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
+        buttons: [
+          {
+            action: "post",
+            label: "Start Game",
+            target: "https://amoba-frame.vercel.app/api/start"
+          }
+        ]
       }
-    }
-  );
+    ]
+  });
 }
