477cf0c3c85b401cfee802f2d23b134e0034e330
Mukedlii <107921705+Mukedlii@users.noreply.github.com>
2025-11-15 08:12:47 +0100

    Update play.js

--- api/play.js
+++ api/play.js
@@ -1,8 +1,15 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "application/json");
-
   res.status(200).json({
-    message: "Your move",
-    board: "empty"
+    frames: [
+      {
+        version: "vNext",
+        image: "https://amoba-frame.vercel.app/957F8E2F-6357-46E3-9B79-386CBEB503BF.png",
+        buttons: [
+          { action: "post", label: "X", target: "https://amoba-frame.vercel.app/api/play" },
+          { action: "post", label: "O", target: "https://amoba-frame.vercel.app/api/play" }
+        ]
+      }
+    ]
   });
 }
